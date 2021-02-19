0c4bfebf4186a18aeb67e5c95ae48908ccd966f6
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-02-19 10:36:34 -0300

    Initial commit

---
+++ src/teste/PrintVariables.java
@@ -0,0 +1,19 @@
+package teste;
+
+public class PrintVariables {
+	public static void main(String[] args) {  
+		
+		String nome = "Danilo Medeiros";
+		int idade = 18;
+		double altura = 1.76;
+		float nascimentoEmHexa = 2002;
+		boolean residente = true;
+		char sexo = 'M';
+		byte numeroCasa = 5;
+		short ultimosDigitosTelefone = 3349;
+		long cpf = 111233344;
+	
+		System.out.println("Me chamo "+nome+" ,tenho "+idade+ " anos e "+altura+" de altura. Nasci em "+nascimentoEmHexa+" com o sexo ("+sexo+"), residente da casa medeiros ("+residente+") numero "+numeroCasa+" tenho como cpf "+cpf+" e final do telefone "+ultimosDigitosTelefone+"!");
+		
+	}
+}
---
+++ src/teste/dog.java
@@ -0,0 +1,7 @@
+package teste;
+
+public class dog {
+	public static void main(String[] args) {
+		System.out.println("E ai, quer dizer... auau");
+	}
+}
