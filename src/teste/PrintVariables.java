141d2a57b51077cfe71ef1b1cda3da6348635826
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-02-22 10:50:58 -0300

    fim das variaveis

--- src/teste/PrintVariables.java
+++ src/teste/PrintVariables.java
@@ -5,7 +5,7 @@ public class PrintVariables {
 		
 		String nome = "Danilo Medeiros";
 		int idade = 18;
-		double altura = 1.76;
+		double altura = 1.75;
 		float nascimentoEmHexa = 2002;
 		boolean residente = true;
 		char sexo = 'M';
@@ -13,7 +13,7 @@ public class PrintVariables {
 		short ultimosDigitosTelefone = 3349;
 		long cpf = 111233344;
 	
-		System.out.println("Me chamo "+nome+" ,tenho "+idade+ " anos e "+altura+" de altura. Nasci em "+nascimentoEmHexa+" com o sexo ("+sexo+"), residente da casa medeiros ("+residente+") numero "+numeroCasa+" tenho como cpf "+cpf+" e final do telefone "+ultimosDigitosTelefone+"!");
+		System.out.println("Me chamo "+nome+" ,tenho "+idade+ " anos e "+altura+" de altura. \nNasci em "+nascimentoEmHexa+" com o sexo ("+sexo+").\nResidente da casa medeiros ("+residente+") numero "+numeroCasa+" tenho como cpf "+cpf+" e final do telefone "+ultimosDigitosTelefone+"!");
 		
 	}
 }
